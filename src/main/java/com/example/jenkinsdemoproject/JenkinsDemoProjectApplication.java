7c5b96afba5464b0c29f7add4184591a3689e121
shahlaparveen <shahla636021@gmail.com>
2022-12-01 15:11:13 +0530

    added jenkins test project

---
+++ src/main/java/com/example/jenkinsdemoproject/JenkinsDemoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.jenkinsdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JenkinsDemoProjectApplication.class, args);
+	}
+
+}
